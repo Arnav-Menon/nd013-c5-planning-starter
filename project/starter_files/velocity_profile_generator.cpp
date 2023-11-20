3a77b610460f98c5b16f1db64c2f9592ca4b6553
Arnav-Menon <arnavmenon@gmail.com>
2023-11-19 21:19:09 -0800

    done

--- project/starter_files/velocity_profile_generator.cpp
+++ project/starter_files/velocity_profile_generator.cpp
@@ -362,7 +362,7 @@ double VelocityProfileGenerator::calc_distance(const double& v_i,
     // v_i (initial velocity) to v_f (final velocity) at a constant
     // acceleration/deceleration "a". HINT look at the description of this
     // function. Make sure you handle div by 0
-    d = ((v_f * v_f) - (V_i * v_i)) / (2 * a);  // <- Updated
+    d = ((v_f * v_f) - (v_f * v_i)) / (2 * a);  // <- Updated
   }
   return d;
 }
@@ -384,7 +384,7 @@ double VelocityProfileGenerator::calc_final_speed(const double& v_i,
   // description of this function. Make sure you handle negative discriminant
   // and make v_f = 0 in that case. If the discriminant is inf or nan return
   // infinity
-  v_f = sqrt(pow(v_i, 2) + 2*d*a);
+  v_f = sqrt(pow(v_i, 2) + 2*a*d);
 
   double disc = pow(v_f, 2) - 4*a*v_i;  // <- Fixed this
   if (disc <= 0.0) {
