023720cf7daf6176a8fe2a133ad2742279ea430d
Arnav-Menon <arnavmenon@gmail.com>
2023-11-19 22:20:54 -0800

    done

--- project/starter_files/velocity_profile_generator.cpp
+++ project/starter_files/velocity_profile_generator.cpp
@@ -362,7 +362,7 @@ double VelocityProfileGenerator::calc_distance(const double& v_i,
     // v_i (initial velocity) to v_f (final velocity) at a constant
     // acceleration/deceleration "a". HINT look at the description of this
     // function. Make sure you handle div by 0
-    d = ((v_f * v_f) - (v_f * v_i)) / (2 * a);  // <- Updated
+    d = ((v_f * v_f) - (v_i * v_i)) / (2 * a);  // <- Updated
   }
   return d;
 }
@@ -386,7 +386,7 @@ double VelocityProfileGenerator::calc_final_speed(const double& v_i,
   // infinity
   v_f = sqrt(pow(v_i, 2) + 2*a*d);
 
-  double disc = pow(v_f, 2) - 4*a*v_i;  // <- Fixed this
+  double disc = pow(v_i, 2) - 2*a*d;  // <- Fixed this
   if (disc <= 0.0) {
     v_f = 0.0;
   } else if (disc == std::numeric_limits<double>::infinity() ||
